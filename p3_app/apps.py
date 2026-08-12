624a635bff9e896aa4305118349546de980043fa
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-08-12 18:43:07 +0330

    Add P3 app config

---
+++ p3_app/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class P3AppConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "p3_app"
+    verbose_name = "A+ Esthetic P3 Community"
