7fae6d6b32a90276594aee30c2929946544adbb9
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-08-12 16:48:08 +0330

    add P0 app config

---
+++ p0_app/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class P0AppConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "p0_app"
+    verbose_name = "A+ Esthetic Production Safety"
