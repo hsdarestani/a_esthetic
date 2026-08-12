64dc7c984e0efaeca4a57a2b963ef2d5cb6fe424
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-08-12 18:10:22 +0330

    Configure P2 Django app

---
+++ p2_app/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class P2AppConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "p2_app"
+    verbose_name = "A+ Esthetic P2 Wallet, Cabinet & Shop"
