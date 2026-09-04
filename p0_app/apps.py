6db55e4a0de23ef15b41372a36f0f16da1de2226
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-09-04 19:16:03 +0330

    Register operational models with Django app registry

--- p0_app/apps.py
+++ p0_app/apps.py
@@ -5,3 +5,6 @@ class P0AppConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "p0_app"
     verbose_name = "A+ Esthetic Production Safety"
+
+    def ready(self):
+        from . import ops_models  # noqa: F401
