aa552a6de13bc38c701a5eb3b9896cc8724f99c5
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-09-04 18:47:41 +0330

    Expose package booking mappings in admin

--- p0_app/admin.py
+++ p0_app/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
-from .models import AccountDeletionRequest, DataExportRequest, DeviceSession
+from .models import (
+    AccountDeletionRequest,
+    DataExportRequest,
+    DeviceSession,
+    PackageBookingRedemption,
+    PackageBookingService,
+)
 
 
 @admin.register(AccountDeletionRequest)
@@ -25,3 +31,18 @@ class DataExportRequestAdmin(admin.ModelAdmin):
     list_filter = ("status",)
     search_fields = ("user__username", "user__email")
     readonly_fields = ("requested_at", "completed_at")
+
+
+@admin.register(PackageBookingService)
+class PackageBookingServiceAdmin(admin.ModelAdmin):
+    list_display = ("package_definition", "service_name", "service_slug", "active", "auto_created")
+    list_filter = ("active", "auto_created")
+    search_fields = ("package_definition__name", "service_name", "service_slug")
+
+
+@admin.register(PackageBookingRedemption)
+class PackageBookingRedemptionAdmin(admin.ModelAdmin):
+    list_display = ("booking_public_id", "user", "member_package", "service_slug", "status", "reserved_at")
+    list_filter = ("status", "service_slug")
+    search_fields = ("booking_public_id", "user__email", "user__username", "member_package__definition__name")
+    readonly_fields = ("booking_public_id", "user", "member_package", "service_slug", "reserved_at", "released_at")
